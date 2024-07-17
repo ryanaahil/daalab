778d84c36df92ab78befbfbe0271cff12158ca93
Ryan Aahil <138552349+ryanaahil@users.noreply.github.com>
2024-07-17 23:53:46 +0530

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,28 @@
+import time
+import random
+
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[high] = array[high], array[i + 1]
+    return i + 1
+
+def quick_sort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quick_sort(array, low, pi - 1)
+        quick_sort(array, pi + 1, high)
+
+#arr = [random.randint(1,90000) for i in range(40000)]
+print("Enter the array")
+arr = list(map(int,input().split()))
+N = len(arr)
+start = time.time()
+quick_sort(arr, 0, N - 1)
+end = time.time()
+print(end-start)
+print(arr)
